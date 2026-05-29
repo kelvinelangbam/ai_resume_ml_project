f6d7c4f72eb11c120465080619f34d113fb49d95
kelvinelangbam <149413338+kelvinelangbam@users.noreply.github.com>
2026-05-30 00:59:47 +0530

    Update suggestions.py

--- utils/suggestions.py
+++ utils/suggestions.py
@@ -2,10 +2,18 @@ def generate_suggestions(score, missing_skills):
     tips = []
 
     if score < 50:
-        tips.append("Improve resume based on job description")
-    if missing_skills:
-        tips.append("Add missing skills: " + ", ".join(missing_skills[:5]))
+        tips.append(
+            "Improve resume based on job description"
+        )
 
-    tips.append("Use action verbs and measurable achievements")
+    if len(missing_skills) > 0:
+        tips.append(
+            "Add missing skills: " +
+            ", ".join(missing_skills[:5])
+        )
+
+    tips.append(
+        "Use action verbs and measurable achievements"
+    )
 
     return tips
